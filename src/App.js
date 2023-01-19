081e9355e94656b3e8bd5047894a0a42182ca74e
Dushyant-sd <114876892+Dushyant-sd@users.noreply.github.com>
2023-01-19 14:46:21 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,74 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import ReactDOM from 'react-dom'
 import './App.css';
+import React, { useState } from 'react';
+// import About from './components/About';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import Alert from './components/Alert';
+// import {
+//   BrowserRouter as Router,
+//   Routes,
+//   Route,
+//   Link
+// } from "react-router-dom";
 
 function App() {
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (msg, type) => {
+    setAlert({
+      message: msg,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 1200);
+  }
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = '#042743';
+      showAlert("Dark mode has been enabled", "success");
+      document.title = "TextUtils - Dark Mode";
+      // setInterval(() => {
+      //   document.title = "TextUtils is Amazing!";
+      // }, 2000);
+      // setInterval(() => {
+      //   document.title = "Install TextUtils Now!";
+      // }, 1500);
+    }
+    else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light mode has been enabled", "success");
+      document.title = "TextUtils - Light Mode"
+    }
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <Router> */}
+        {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
+        <Navbar title='TextUtils' mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          {/* <Routes> */}
+            {/* <Route exact path="/about" element={<About />} />
+            <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/>} /> */}
+            {/* using exact path will make sure react will go to exact path (not partial), example:- 
+            /users --> Component 1
+            /users/home --> Component 2
+            both components are different but if we don't use 'exact' react will redirect to Component 1  */}
+          {/* </Routes> */}
+          <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode}/>
+          {/* <About /> */}
+        </div>
+      {/* </Router> */}
+    </>
   );
 }
 
