05607d796000c649a3a92ba8f4ca4e5234ecaeff
Dushyant-sd <114876892+Dushyant-sd@users.noreply.github.com>
2023-01-19 22:11:45 +0530

    Updates

---
+++ precache-manifest.bc0a293f120ca4da2a6938b310395638.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9bb9c0863871b022a7e4",
+    "url": "/my-app/static/css/main.5e7648f6.chunk.css"
+  },
+  {
+    "revision": "9bb9c0863871b022a7e4",
+    "url": "/my-app/static/js/main.cfd4c60a.chunk.js"
+  },
+  {
+    "revision": "a9e9488bf1c79412ffe2",
+    "url": "/my-app/static/js/runtime~main.ad3febe6.js"
+  },
+  {
+    "revision": "32d9fe79fc3fed3a7c60",
+    "url": "/my-app/static/js/2.39313b4f.chunk.js"
+  },
+  {
+    "revision": "ef276a8df7b31e3a5070",
+    "url": "/my-app/static/js/3.d62f7fe7.chunk.js"
+  },
+  {
+    "revision": "1c979772ffa3493a8582551930474eb5",
+    "url": "/my-app/index.html"
+  }
+];
